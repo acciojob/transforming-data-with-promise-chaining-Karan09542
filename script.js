362ad55777db496fe6b6c640fb5e779122c30001
Karan vishwakarma <150724533+Karan09542@users.noreply.github.com>
2025-09-24 21:34:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const input = document.getElementById("ip")
+const btn = document.getElementById("btn")
+const output = document.getElementById("output")
+
+const updateDom = (value, delay=1000)=>{
+	return new Promise((resolve)=> {
+		setTimeout(()=> {
+			output.innerHTML = value;
+			resolve(value)
+		},delay)
+	})
+}
+
+btn.addEventListener("click", ()=> {
+	const value = Number(input.value)
+	 updateDom(value, 2000)
+	.then((value) => updateDom(value*2, 2000))
+	.then((value) => updateDom(value-3),5559)
+	.then((value) => updateDom(value/2),9999)
+	.then((value) => updateDom(value+10))
+})
